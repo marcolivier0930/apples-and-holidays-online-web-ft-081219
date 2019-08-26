ecc4c379c43a9e2d884900294a6d563b189c4992
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-26 16:50:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,7 +52,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  id_bbq_season_values = holiday_hash.coolect {|season, holidays|
 end
 
 
